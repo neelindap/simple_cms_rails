63215d86227a74e68cd67e1486098d6e58c0f491
Neel Indap <neelindap@gmail.com>
2018-10-02 23:39:28 -0400

    CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'subject/index'
 
-  get 'demo/index'
-
-  match ':controller/(:action/(:id))', :via => :get
+  root 'demo#index'
+  match ':controller/(:action/(:id))', :via => [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
